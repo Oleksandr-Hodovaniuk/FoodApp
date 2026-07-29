6988f6b59e979d22f3447bce3bc7f6671ac5ed87
Oleksander Hodovaniuk <s.godovanuk@gmail.com>
2026-07-29 15:06:05 +0300

    Add more layers and Scalar

---
+++ backend/Catalog/src/Catalog/ConfigureServices.cs
@@ -0,0 +1,28 @@
+﻿using Scalar.AspNetCore;
+
+namespace Catalog;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.MapScalarApiReference(options =>
+            {
+                options.WithOpenApiRoutePattern("/swagger/{documentName}/swagger.json");
+            });
+        }
+
+        return app;
+    }
+}
--- backend/Catalog/src/Catalog/Program.cs
+++ backend/Catalog/src/Catalog/Program.cs
@@ -1,12 +1,16 @@
+using Catalog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddApiServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseApiServices();
 
 app.UseHttpsRedirection();
 
